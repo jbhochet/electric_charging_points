34c6d857a1a1ecf122d618618d431c6f6b36e92a
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-11-03 17:48:48 +0100

    implementation of the graph class methods

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,18 +1,79 @@
 package graph;
 
+/**
+ * Represents an unordered graph with an adjacency matrix.
+ */
 public class Graph {
-    private int[][] matrix;
 
+    /** 
+     * The adjacency matrix of this graph
+     */
+    private final boolean[][] matrix;
+
+    /**
+     * Create a new Graph object with the specified order.
+     * @param order The graph order
+     */
     public Graph(int order) {
+        // init value is false
+        matrix = new boolean[order][order];
     }
 
+    /**
+     * Check if x and y are adjacent in this graph.
+     * @param x
+     * @param y
+     * @return true if x and y are adjacent, false otherwise.
+     */
     public boolean adjacent(int x, int y) {
-        return false;
+        return matrix[x][y];
     }
 
+    /**
+     * Get the number of neighbors of v.
+     * @param v The vertex.
+     * @return The number of neighbors.
+     */
+    private int nbNeighbors(int v) {
+        int res = 0;
+        for(int w=0;w< matrix.length;w++)
+            if(adjacent(v, w)) res++;
+        return res;
+    }
+
+
+    /**
+     * Get all the neighbors of vertex v.
+     * @param v The vertex.
+     * @return An array of the neighbors of v.
+     */
+    public int[] neighbors(int v) {
+        int i=0;
+        int[] res = new int[nbNeighbors(v)];
+
+        for(int w=0;w< matrix.length;w++)
+            if(adjacent(v,w)) res[i++] = w;
+
+        return res;
+    }
+
+    /**
+     * Create an edge between x and y.
+     * @param x The first vertex
+     * @param y The second vertex
+     */
     public void addEdge(int x, int y) {
+        matrix[x][y] = true;
+        matrix[y][x] = true;
     }
 
+    /**
+     * Remove the edge between x and y.
+     * @param x The first vertex.
+     * @param y The second vertex.
+     */
     public void removeEdge(int x, int y) {
+        matrix[x][y] = false;
+        matrix[y][x] = false;
     }
 }
