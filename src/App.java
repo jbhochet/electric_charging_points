b56fb64ce1100cb416833b43b5c1e1e8f986f321
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-11-08 18:34:08 +0100

    create constructor and methods readInt and readCities in class Cli

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import ui.Cli;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Cli cli = new Cli(sc);
+
         System.out.println("Hello World!");
+
+        cli.start();
+
+        sc.close();
     }
 }
--- src/ui/Cli.java
+++ src/ui/Cli.java
@@ -3,12 +3,77 @@ package ui;
 import core.CA;
 import core.City;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Cli {
-    
+    /**
+     * An instance of Scanner used to read user input.
+     */
+    private Scanner sc;
+
+    /**
+     * The agglomeration community to work with.
+     */
     private CA ca;
 
+    /**
+     * Create a new instance with this Scanner used to read input.
+     * @param sc The Scanner instance for reading input.
+     */
+    public Cli(Scanner sc) {
+        this.sc = sc;
+    }
+
+    /**
+     * Get an integer from the user.
+     * @param message The message that prints on the console for the user.
+     * @return The integer entered by the user.
+     */
+    private int readInt(String message) {
+        int res = 0;
+        boolean valid = false;
+
+        while (!valid) {
+            try {
+                System.out.println(message);
+                res = sc.nextInt();
+                valid = true;
+            } catch (InputMismatchException err) {
+                System.out.println("You must provide an integer!");
+                sc.nextLine();
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * Get cities from the user and return an array.
+     * @return The array of cities.
+     */
     private City[] readCities() {
-        return null;
+        int nbCities;
+        City[] cities;
+
+        // Ask the number of cities
+        do {
+            nbCities = readInt("How many city do you want to have?");
+            // Check if the number is valid
+            if(nbCities < 1 || nbCities > 26) {
+                System.out.println("The number of cities must be between 1 and 26 included!");
+                nbCities = 0; // We will ask again
+            }
+        } while (nbCities == 0);
+
+        cities = new City[nbCities];
+
+        // Create cities with letters
+        for(char c='A'; c<'A'+nbCities; c++) {
+            cities[c-'A'] = new City(String.valueOf(c));
+        }
+
+        return cities;
     }
 
     private void roadManagerMenu() {}
@@ -17,5 +82,18 @@ public class Cli {
 
     private void showCitiesWithChargingPoint() {}
 
-    public void start() {}
+    /**
+     * Start the command line interface to interact with the user.
+     */
+    public void start() {
+        City[] cities;
+
+        // Get cities from the user
+        cities = readCities();
+
+        // Create CA
+
+        // Launch road manager menu
+        roadManagerMenu();
+    }
 }
